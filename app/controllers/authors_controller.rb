78d23859c7b8aa3d90d7196190580bafb3a26ac9
Reece_Collett <rcollett@spartaglobal.com>
2018-08-29 20:28:48 +0100

    Created controllers for books and authors

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,25 @@
+class AuthorsController < ApplicationController
+  def index
+  end
+
+  def all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def one
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'authors/index'
+  get 'authors/all'
+  get 'authors/create'
+  get 'authors/new'
+  get 'authors/edit'
+  get 'authors/update'
+  get 'authors/delete'
+  get 'authors/one'
+  get 'books/index'
+  get 'books/all'
+  get 'books/create'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/delete'
+  get 'books/one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
