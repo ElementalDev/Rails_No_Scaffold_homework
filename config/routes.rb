d1fc7c79a2644f9a414ffb259c0121585430f544
Reece_Collett <rcollett@spartaglobal.com>
2018-08-29 20:31:54 +0100

    Added resources to routes and routed to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
-  get 'authors/index'
-  get 'authors/all'
-  get 'authors/create'
-  get 'authors/new'
-  get 'authors/edit'
-  get 'authors/update'
-  get 'authors/delete'
-  get 'authors/one'
-  get 'books/index'
-  get 'books/all'
-  get 'books/create'
-  get 'books/new'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/delete'
-  get 'books/one'
+  resources :books
+  resources :author
+
+  get "/", to: "authors#index", as: "home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
